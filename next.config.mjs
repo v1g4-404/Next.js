93ee10ed4232b49987d43f2a55c43ae706c98367
眞野初芽 <manohatsume@manohatsumenoMacBook-Pro.local>
2025-12-18 14:33:31 +0900

    課題８実装

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.jp",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
